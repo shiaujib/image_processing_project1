41640794a834571ce4604d3156851045538a6e1b
shiaujib <ch23173885@yahoo.com.tw>
2015-10-12 19:03:08 +0800

    finish bilinear interpolation

--- project1.cpp
+++ project1.cpp
@@ -23,32 +23,77 @@ double min(double x,double y){
 		return x;
 }
 
+void bilinear(float ratio){
+        uchar* dataDst=matSdst.data;
+	uchar* dataSrc=matDst.data;
+	int stepDst=matSdst.step;
+	int stepSrc=matDst.step;
+	double fx,fy;
+        int ix,iy;
+        for(int j=0;j<matSdst.rows;j++)
+                for(int i=0;i<matSdst.cols;i++){
+			fx=(i+0.5)*ratio-0.5;
+			fy=(j+0.5)*ratio-0.5;
+                        ix=cvRound(fx);
+		        fx-=ix;
+		        iy=cvRound(fy);
+			fy-=iy;
+		/*	if(ix<0){
+				fx=0;
+				ix=0;
+			}
+			if(ix>matSdst.cols-1)
+				fx=0,ix=matSdst.cols-2;
+			if(iy<0){
+				fy=0;
+				iy=0;
+			}
+			if(iy>matSdst.rows-1)
+				fx=0,iy=matSdst.rows-2;
+		//	fx=(short)fx;
+		//	fy=(short)fy; */
+			for(int k=0;k<matSdst.channels();k++){
+				*(dataDst+j*stepDst+3*i+k)=(*(dataSrc+iy*stepSrc+3*ix+k)*(1-fx)*(1-fy)+
+				*(dataSrc+(iy+1)*stepSrc+3*ix+k)*(1-fx)*fy+
+				*(dataSrc+iy*stepSrc+3*(ix+1)+k)*fx*(1-fy)+
+				*(dataSrc+(iy+1)*stepSrc+3*(ix+1)+k)*fx*fy);
+                          }
+                    }
+         imshow("bilinear",matSdst);
+	 waitKey(0);
+	  }
 
 
-void scale(double ratio)
+void scale(double ratio,int algo)
 {	
 	int srcx,srcy;
 	double sdx,sdy;
 	double x_ratio,y_ratio;
-	cout<<matDst.cols<<endl;
-	cout<<matDst.rows<<endl;
-//	matSrc=imread("test.tif",1);
+//	cout<<matDst.cols<<endl;
+//	cout<<matDst.rows<<endl;
+//	matSrc=imread("lena.jpg",1);
 //	imshow("input",matSrc);
+	waitKey(0);
 //	cout<<"input height*width"<<matSrc.rows<<"*"<<matSrc.cols<<endl;
 	matSdst=Mat(Size(matSrc.cols*ratio,matSrc.rows*ratio),matDst.type());
 	cout<<"onput height*width"<<matDst.rows<<"*"<<matDst.cols<<endl;
 	x_ratio=(double)matSrc.cols/matSdst.cols;
 	y_ratio=(double)matSrc.rows/matSdst.rows;
-	for(int i=0;i<matSdst.cols;i++)
-		for(int j=0;j<matSdst.rows;j++)
-			{
-				sdx=i*x_ratio;
-				sdy=j*y_ratio;
-				near(&sdx,&sdy);
-				matSdst.at<Vec3b>(j,i)=matDst.at<Vec3b>(sdy,sdx);
-			}
-	imshow("Result",matSdst);
-	waitKey(0);
+	cout<<x_ratio<<"     "<<y_ratio;
+	if(algo==0){
+		for(int i=0;i<matSdst.cols;i++)
+			for(int j=0;j<matSdst.rows;j++)
+				{
+					sdx=i*x_ratio;
+					sdy=j*y_ratio;
+					near(&sdx,&sdy);
+					matSdst.at<Vec3b>(j,i)=matDst.at<Vec3b>(sdy,sdx);
+				}
+		imshow("nearest neighbor scale",matSdst);
+		waitKey(0);
+	}
+	if(algo==1)
+		bilinear(x_ratio);
 } 
 
 /*void scale_rotate(double ratio,double andle)
@@ -79,7 +124,7 @@ void scale(double ratio)
 
 void rotate(double angle){
 	double rx,ry;
-	angle=angle*CV_PI/180;
+	angle=-angle*CV_PI/180;
 	
 	matSrc=imread("test.tif",1);
 	matDst=Mat(matSrc.size(),matSrc.type());
@@ -97,23 +142,17 @@ void rotate(double angle){
 			else
 				matDst.at<Vec3b>(j,i)=matSrc.at<Vec3b>(ry,rx);
 				}
-	imshow("Result",matDst);
-	waitKey(0);
 		
 		
 }
-void traslation_shear(double x,double y,double shear,bool type){
+void traslation_shear(double x,double y,double shear,bool type,int algo){
 	
 	double tx,ty;
-//	Mat matSrc,matDst;
-//	matSrc=imread("test.tif",1);
-//	matDst=Mat(matSrc.size(),matSrc.type());
-//	imshow("input",matSrc);
-//	waitKey(0);
 	matTdst=Mat(matSdst.size(),matSdst.type());
-	for(int i=0;i<matSdst.cols;i++)
-		for(int j=0;j<matSdst.rows;j++){
-			if(type==1){	
+	if(algo==0){
+		for(int i=0;i<matSdst.cols;i++)
+			for(int j=0;j<matSdst.rows;j++){
+				if(type==1){	
 				tx=((i-0.5*matSdst.cols)-x+shear*(j-0.5*matSdst.rows));
 				ty=(j-0.5*matSdst.rows)+y;
 				tx+=0.5*matSdst.cols;
@@ -124,7 +163,7 @@ void traslation_shear(double x,double y,double shear,bool type){
 				else
 					matTdst.at<Vec3b>(j,i)=matSdst.at<Vec3b>(ty,tx);
 				}
-			else{
+				else{
 				tx=((i-0.5*matSdst.cols)-x);
 				ty=(j-0.5*matSdst.rows)+y+shear*(i-0.5*matSdst.cols);
 				tx+=0.5*matSdst.cols;
@@ -141,10 +180,13 @@ void traslation_shear(double x,double y,double shear,bool type){
 	imshow("Transform+shear",matTdst);
 	waitKey(0);
 	}
+	/*if(algo==1)
+		bilinear(1.5);*/
+	}
 
 
 int main(){
-	double scaler,angle,tranx,trany,shear,type;
+	/*double scaler,angle,tranx,trany,shear,type;
 	cout<<"enter the scaling ratio:"<<endl;
 	cin>>scaler;
 	cout<<"enter rotation angle"<<endl;
@@ -158,9 +200,12 @@ int main(){
 	rotate(angle);
 	scale(scaler);
 	traslation_shear(tranx,trany,shear,type);
-//	nearest_neighbor(x);
-//	rotate(x);
-//	traslation(x,x,shear);
+	rotate(0);
+//	scale(10,1);*/
+	rotate(0);
+	scale(1.5,1);
+	traslation_shear(0,0,0.4,1,0);
+
 	return 0;	
 
 
